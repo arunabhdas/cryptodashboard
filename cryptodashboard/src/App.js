0a421887c13285af2e5e56e2cddef73383dc8f43
Arunabh Das <arunabhdas@gmail.com>
2018-10-08 05:08:04 -0400

    Added Card to App component

--- cryptodashboard/src/App.js
+++ cryptodashboard/src/App.js
@@ -1,25 +1,23 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import "tabler-react/dist/Tabler.css";
+
+import { Card, Button } from "tabler-react";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Card>
+          <Card.Header>
+            <Card.Title>CryptoDashboard</Card.Title>
+          </Card.Header>
+          <Card.Body>
+            <Button color="primary">Welcome</Button>
+          </Card.Body>
+        </Card>
       </div>
     );
   }
